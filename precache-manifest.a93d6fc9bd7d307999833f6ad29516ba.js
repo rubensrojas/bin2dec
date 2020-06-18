024af8b140c217144ba08b8a6c9f88201603ebed
rubensrojas <rubensarojas@gmail.com>
2020-06-18 12:17:28 -0300

    Updates

---
+++ precache-manifest.a93d6fc9bd7d307999833f6ad29516ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7d257bb6e2738176b608ea4efc8493",
+    "url": "/bin2dec/index.html"
+  },
+  {
+    "revision": "831b282d768371fe7852",
+    "url": "/bin2dec/static/css/main.27ab2a7f.chunk.css"
+  },
+  {
+    "revision": "6a77cd495f1b54f7dc3a",
+    "url": "/bin2dec/static/js/2.b1352390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bin2dec/static/js/2.b1352390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831b282d768371fe7852",
+    "url": "/bin2dec/static/js/main.668707f8.chunk.js"
+  },
+  {
+    "revision": "ff074e63912f184f4594",
+    "url": "/bin2dec/static/js/runtime-main.1d1ad8e8.js"
+  }
+]);
